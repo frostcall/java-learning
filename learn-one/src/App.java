59bf7b6082188f1d25ce072bedc6dde651cd6b6a
Me <me@me.com>
2023-07-05 14:21:00 -0400

    first stuff

---
+++ learn-one/src/App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.print("\tNew Line printed.\n");
+        System.out.println("Another new Line printed.");
+
+        int newInt = 123;
+        boolean newBool = true;
+        float newFloat = 3.14f;
+        long newLong = 1223123121123123L;
+        double newDouble = 3.14;
+        String newString = "Hello there!";
+
+
+        System.out.println("Variables: "+newInt +" "+newBool+" "+newFloat+" "+newLong+" "+newDouble);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+        int newAge = age + 1;
+
+        System.out.println("Hello "+name);
+        System.out.println("Did you say you were "+newAge+" years old?");
+    }
+}
